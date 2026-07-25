a580c62e22f54d3cec50f6b4e29f9150c6187a82
stefanhiemer <StefanHiemer@web.de>
2026-07-25 17:57:36 +0200

    enable editable install via pip.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='THBSplines',
-    version='',
+    version='0.1.0',
     packages=['THBSplines', 'THBSplines.src', 'THBSplines.src_c'],
     url='',
     license='',
